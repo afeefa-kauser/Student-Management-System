c8bb2c3ef53d24a345facd611a6b610983facd58
Afeefa Kauser <afeefakauser407@gmail.com>
2026-07-02 18:21:02 +0530

    Add sample data for Students, Courses, and Enrollment

---
+++ sample_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO Students VALUES
+(1,'Ali',20,'Computer Science','ali@gmail.com'),
+(2,'Sara',21,'Electronics','sara@gmail.com'),
+(3,'John',22,'Mechanical','john@gmail.com');
+
+INSERT INTO Courses VALUES
+(101,'Database Systems'),
+(102,'Python Programming');
+
+INSERT INTO Enrollment VALUES
+(1,1,101),
+(2,2,102),
+(3,3,101);
